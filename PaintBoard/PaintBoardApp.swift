bd60d05c1e82f9bca3b2b9029e2a2565929ab18d
deblur99 <deblur99@kakao.com>
2023-08-17 13:43:40 +0900

    Initial Commit

---
+++ PaintBoard/PaintBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintBoardApp.swift
+//  PaintBoard
+//
+//  Created by 한현민 on 2023/08/17.
+//
+
+import SwiftUI
+
+@main
+struct PaintBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
